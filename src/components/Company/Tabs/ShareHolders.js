d8fc62410f725efcba4a9be1bf7e19b4875467e6
Sergey Khmelevskoy <xs290@me.com>
2018-10-12 23:19:49 +0300

    add template for tables

--- src/components/Company/Tabs/ShareHolders.js
+++ src/components/Company/Tabs/ShareHolders.js
@@ -1,11 +1,88 @@
 import React, {Component} from 'react';
+import ShareholderTable from '../ShareholderTable';
 
 class ShareHolders extends Component{
-  render(){
-    return(
-      <div className="company__tab">
+  constructor(){
+    super()
 
-      </div>
+    this.state = {
+      shareholders_corporate: ' '
+    }
+  }
+
+  // called from the parent onBlur or checkbox onClick
+  updateState = (name, componentState) => {
+    this.setState({ ...this.state,
+      [name]: componentState
+    })
+  }
+
+  render(){
+
+    const individualsTable = {
+      thead: [
+        {
+          icon: "sh-name",
+          name: "Full name"
+        },
+        {
+          icon: "sh-id",
+          name: "ID"
+        },
+        {
+          icon: "sh-phone",
+          name: "Phone number"
+        },
+        {
+          icon: "sh-email",
+          name: "Email"
+        },
+        {
+          name: "Shareholder?"
+        },
+        {
+          name: "Director?"
+        }
+
+      ],
+      tbody: [
+        {
+          type: "input",
+          placeholder: "Full name",
+          name: "full_name"
+        },
+        {
+          type: "input",
+          placeholder: "Insert ID",
+          name: "id_number"
+        },
+        {
+          type: "input",
+          placeholder: "Insert number",
+          name: "phone_number"
+        },
+        {
+          type: "input",
+          placeholder: "Insert email",
+          name: "email"
+        },
+        {
+          type: "checkbox",
+          name: "is_shareholder"
+        },
+        {
+          type: "checkbox",
+          name: "is_director"
+        }
+      ]
+    }
+
+    return(
+      <ShareholderTable
+        title="List of relevant corporate entities (i.e. corporate shareholders or directors)"
+        schema={individualsTable}
+        updateState={this.updateState.bind(this, "shareholders_corporate")}
+      />
     )
   }
 }
--- src/components/Company/Tabs/StakeHolders.js
+++ src/components/Company/Tabs/StakeHolders.js
@@ -1,11 +1,144 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import ShareholderTable from '../ShareholderTable';
 
 class StakeHolders extends Component{
-  render(){
-    return(
-      <div className="company__tab">
+  constructor(){
+    super()
 
-      </div>
+    this.state = {
+      shareholders_individulas: '',
+      shareholders_corporate: ' '
+    }
+  }
+
+  // called from the parent onBlur or checkbox onClick
+  updateState = (name, componentState) => {
+    this.setState({ ...this.state,
+      [name]: componentState
+    })
+  }
+
+  render(){
+
+    const individualsTable = {
+      thead: [
+        {
+          icon: "sh-name",
+          name: "Full name"
+        },
+        {
+          icon: "sh-id",
+          name: "ID"
+        },
+        {
+          icon: "sh-phone",
+          name: "Phone number"
+        },
+        {
+          icon: "sh-email",
+          name: "Email"
+        },
+        {
+          name: "Shareholder?"
+        },
+        {
+          name: "Director?"
+        }
+
+      ],
+      tbody: [
+        {
+          type: "input",
+          placeholder: "Full name",
+          name: "full_name"
+        },
+        {
+          type: "input",
+          placeholder: "Insert ID",
+          name: "id_number"
+        },
+        {
+          type: "input",
+          placeholder: "Insert number",
+          name: "phone_number"
+        },
+        {
+          type: "input",
+          placeholder: "Insert email",
+          name: "email"
+        },
+        {
+          type: "checkbox",
+          name: "is_shareholder"
+        },
+        {
+          type: "checkbox",
+          name: "is_director"
+        }
+      ]
+    }
+
+    const corporatesTable = {
+      thead: [
+        {
+          icon: "sh-name",
+          name: "Company name"
+        },
+        {
+          icon: "sh-id",
+          name: "UEN"
+        },
+        {
+          icon: "sh-person",
+          name: "Name of corporate representative"
+        },
+        {
+          name: "Shareholder?"
+        },
+        {
+          name: "Director?"
+        }
+
+      ],
+      tbody: [
+        {
+          type: "input",
+          placeholder: "Company name",
+          name: "company_name"
+        },
+        {
+          type: "input",
+          placeholder: "UEN",
+          name: "uen"
+        },
+        {
+          type: "input",
+          placeholder: "Corporate representative",
+          name: "corporate_representative"
+        },
+        {
+          type: "checkbox",
+          name: "is_shareholder"
+        },
+        {
+          type: "checkbox",
+          name: "is_director"
+        }
+      ]
+    }
+
+    return(
+      <Fragment>
+        <ShareholderTable
+          title="List of key individuals (shareholders and directors)"
+          schema={individualsTable}
+          updateState={this.updateState.bind(this, "shareholders_individulas")} />
+
+        <ShareholderTable
+          title="List of relevant corporate entities (i.e. corporate shareholders or directors)"
+          schema={corporatesTable}
+          updateState={this.updateState.bind(this, "shareholders_corporate")} />
+      </Fragment>
     )
   }
 }
